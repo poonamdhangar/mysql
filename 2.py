43df64459c6fb0dd50ae8a7c25d108d9af8e9985
poonam dhangar <poonamkumar9098@gmail.com>
2021-12-20 02:38:14 -0800

    F

---
+++ 2.py
@@ -0,0 +1,10 @@
+import poonam
+# poonam.func()
+# from poonam import MyClass
+# obj=MyClass()
+# obj.hello()
+from poonam import MyClassA
+# obj=MyClass()
+# obj.hello()
+obj1=MyClassA() 
+obj1.sayhello()
---
+++ poonam.py
@@ -0,0 +1,19 @@
+def func():
+    print("hello Function How are you")
+func()
+class MyClass():
+    class MyClassA():
+        
+        def sayhello(self):
+            print("Hello Sayhello Class")
+        def hello(self):
+            print("HEllo Hello mathod")
+
+        def sayhello(self):
+            a=10
+            b=20
+            print("Hello Sayhello Class")
+obj1=MyClassA() 
+obj1.sayhello()
+
+    
